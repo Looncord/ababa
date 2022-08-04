c6de8d3476be173c1f3f943f6f5d0985d79d8309
Looncord <107969119+Looncord@users.noreply.github.com>
2022-08-04 23:58:45 +0300

    Add files via upload

---
+++ test.lua
@@ -0,0 +1,7 @@
+if game.PlaceId == 8396586868 then
+    local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+    local Window = Library.CreateLib("AOPG v1", "KL v1")
+
+    --Main
+    local Main = Window:NewTab("Main")
+    local MainSelection = Main:NewSelection("Main")
